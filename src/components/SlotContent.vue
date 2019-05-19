1c55d2bdf4b90ed0fbba9dd5748eeb60156e809d
koyo <nexus9071@gmail.com>
2019-05-20 02:06:39 +0900

    スロット実装

--- src/components/SlotContent.vue
+++ src/components/SlotContent.vue
@@ -2,12 +2,17 @@
   <v-container>
     <v-layout text-xs-center wrap>
       <v-flex xs12>
-        <v-img :src="resultImg" class="my-3" contain height="400"></v-img>
+        <v-img :src="selectedImage" class="my-3" contain height="400"></v-img>
       </v-flex>
 
       <v-flex mb-4>
         <h1 class="font-weight-bold">
-          <v-btn color="success">Slot Start !!</v-btn>
+          <span v-if="!running">
+            <v-btn color="success" @click="startSlot">Slot Start !!</v-btn>
+          </span>
+          <span v-else>
+            <v-btn color="error" @click="stopSlot">Stop !!</v-btn>
+          </span>
         </h1>
       </v-flex>
     </v-layout>
@@ -17,13 +22,30 @@
 <script>
 export default {
   data: () => ({
-    resultImg: require(`@/assets/icon.png`),
-    resultImgs: []
+    selectedImage: require(`@/assets/icon.png`),
+    images: [],
+    timeId: 0,
+    running: false
   }),
-  mounted() {
-    for (let i = 1; i < 11; i++) {
-      this.resultImgs.push(require(`@/assets/slot_images/image_${i}.png`));
+  methods: {
+    getImageData() {
+      for (let i = 1; i < 11; i++) {
+        this.images.push(require(`@/assets/slot_images/image_${i}.png`));
+      }
+    },
+    startSlot() {
+      var imageIdx = Math.floor(Math.random() * this.images.length);
+      this.selectedImage = this.images[imageIdx];
+      this.timeId = setTimeout(this.startSlot, 50);
+      this.running = true;
+    },
+    stopSlot() {
+      clearTimeout(this.timeId);
+      this.running = false;
     }
+  },
+  mounted() {
+    this.getImageData();
   }
 };
 </script>
